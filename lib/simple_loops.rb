36d6358deb05fb86dfcc7bbddaecc7eeb8c15112
Niketa Jenerette <niketa.jenerette@gmail.com>
2020-05-09 04:29:37 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,15 +1,20 @@
 
 def loop_message_five_times(message) 
-message= "Hello World."
-5.times do
+count=0 
+while count<5 do 
   puts (message)
+  count += 1 
+  end
 end
 
 def loop_message_n_times (message, number)
   message= "Hello Moon."
   n=5
-  count=
-  
+  count=0 
+  while count < number do
+    puts (message)
+    count +=1 
+  end
 end
   
 def output_array
